1d2b97c9bd181362bb4ef58a97b62e64ea92b42c
Rakaneth <rakaneth@gmail.com>
2019-04-10 17:56:48 -0400

    the beginnings of greatness

--- great-taming-dotnet/Program.cs
+++ great-taming-dotnet/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SadConsole;
+using Microsoft.Xna.Framework;
+using Console = SadConsole.Console;
 
-namespace great_taming_dotnet
+namespace GreatTaming
 {
     class Program
     {
         static void Main(string[] args)
         {
+            SadConsole.Game.Create(100, 40);
+            SadConsole.Game.OnInitialize = Init;
+            SadConsole.Game.Instance.Run();
+            SadConsole.Game.Instance.Dispose();
+        }
+
+        static void Init()
+        {
+            var console = new Console(100, 40);
+            console.FillWithRandomGarbage();
+            console.Fill(new Rectangle(3, 3, 23, 3), Color.Violet, Color.Black, 0, 0);
+            console.Print(4, 4, "Hello, SadConsole!");
+            SadConsole.Global.CurrentScreen = console;
         }
     }
 }
---
+++ great-taming-dotnet/UI.cs
@@ -0,0 +1,28 @@
+﻿using GreatTaming.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatTaming.UI
+{
+    abstract class UI
+    {
+        string Name { get;  }
+        public UI(string name)
+        {
+            Name = name;
+        }
+        public abstract void render();
+        public abstract ICommand handle();
+        public void enter()
+        {
+            System.Console.WriteLine($"Entered {Name} screen.");
+        }
+        public void exit()
+        {
+            System.Console.WriteLine($"Exited {Name} screen.");
+        }
+    }
+}
