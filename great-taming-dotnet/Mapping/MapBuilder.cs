23629bc4793af2fb2d36a76a943da4b916fc5b0a
Rakaneth <rakaneth@gmail.com>
2019-04-18 01:03:30 -0400

    format mapbuilder

--- great-taming-dotnet/Mapping/MapBuilder.cs
+++ great-taming-dotnet/Mapping/MapBuilder.cs
@@ -1,42 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using GoRogue;
 using GoRogue.GameFramework;
 using GoRogue.MapGeneration;
 using GoRogue.MapViews;
 using GreatTaming.Engine;
 using GreatTaming.Entity;
-using GoRogue;
 using Microsoft.Xna.Framework;
 
-namespace GreatTaming.Mapping
-{
-    public enum MapType
-    {
+namespace GreatTaming.Mapping {
+    public enum MapType {
         DUNGEON,
         CAVES
     }
 
-    public static class MapBuilder
-    {
-        public static Map Build(int width, int height, string id, GameContext context, MapType typ)
-        {
+    public static class MapBuilder {
+        public static Map Build(int width, int height, string id, GameContext context, MapType typ) {
             var baseMap = new ArrayMap<Terrain>(width, height);
             var trans = new TerrainTranslator(baseMap, Color.Gray, Color.LightGray);
 
             List<Coord> doors = new List<Coord>();
-            switch (typ)
-            {
+            switch (typ) {
                 case MapType.DUNGEON:
                     var dunResult = QuickGenerators.GenerateDungeonMazeMap(trans, context.RNG, 10, 50, 5, 21);
-                    foreach (var (room, connectors) in dunResult)
-                    {
-                        foreach (var side in connectors)
-                        {
-                            foreach (var door in side)
-                            {
+                    foreach (var (room, connectors) in dunResult) {
+                        foreach (var side in connectors) {
+                            foreach (var door in side) {
                                 doors.Add(door);
                             }
                         }
@@ -46,7 +35,7 @@ namespace GreatTaming.Mapping
                     var caveResult = QuickGenerators.GenerateCellularAutomataMap(trans, context.RNG);
                     break;
             }
-            
+
             var fullMap = Map.CreateMap(baseMap, 4, Distance.CHEBYSHEV);
             foreach (var newDoorSpot in doors) {
                 fullMap.SetTerrain(Terrain.ClosedDoor(newDoorSpot));
